fa439e7521a42949191774b7c811c5ce87a1f3e5
Fernando Bacelar <fernando_bac97@hotmail.com>
2022-01-26 08:36:23 -0300

    Added initial dictionary comment

--- projeto2.py
+++ projeto2.py
@@ -3,6 +3,7 @@ import time
 import pandas as pd
 import numpy as np
 
+#TO DO: Create cities dictionary
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
